fba049e333e7be2e4ae7f90d0b61d627cf699b0b
Andrea Amorosi <dreamorosi@gmail.com>
2017-03-26 05:06:27 +0200

    :tada: Initial commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <div>
+          <h2>Welcome to React</h2>
+        </div>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Home
